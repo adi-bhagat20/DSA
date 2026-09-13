41b66de9f56f32b0a014839d94986dab01923c87
Aditya Viraj Bhagat <adityavirajbhagat@gmail.com>
2026-09-13 06:54:25 -0700

    Time: 12 ms (55.18%) | Memory: 19.3 MB (76.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,14 +5,9 @@ class Solution:
         
         d = {}
 
-        for ch in s:
-            d[ch] = d.get(ch , 0) + 1
-        
-        for ch in t:
-            if ch not in d:
-                return False
-            else:
-                d[ch] -= 1
+        for i in range(len(s)):
+            d[s[i]] = d.get(s[i] , 0) + 1
+            d[t[i]] = d.get(t[i] , 0) - 1
             
         for val in d.values():
             if val != 0:
