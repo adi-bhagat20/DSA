375af3f5b4a868fb389839010826594d429d6e4f
Aditya Viraj Bhagat <adityavirajbhagat@gmail.com>
2026-09-13 06:56:54 -0700

    Time: 15 ms (43.32%) | Memory: 19.4 MB (76.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,13 +3,13 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        d = {}
+        arr = [0]*26
 
         for i in range(len(s)):
-            d[s[i]] = d.get(s[i] , 0) + 1
-            d[t[i]] = d.get(t[i] , 0) - 1
-            
-        for val in d.values():
+            arr[ord(s[i]) - ord('a')] += 1
+            arr[ord(t[i]) - ord('a')] -= 1
+        
+        for val in arr:
             if val != 0:
                 return False
             
